db1bde88baabb74c60f7967950cb7f78ebf08fc5
Vorobev Ivan <vorobey92@yandex.ru>
2017-04-10 00:17:31 +0300

    task-26 переделал копирование скриптов (в продакшене будем копировать из джарника - повод от них избавиться) убрал логирование в файл, так как спринг предоставляет фитчу логирования в /var/log/... после установки его как сервис

--- app/src/main/java/ru/hh/safebox/app/Sandbox.java
+++ app/src/main/java/ru/hh/safebox/app/Sandbox.java
@@ -5,8 +5,17 @@ import org.slf4j.LoggerFactory;
 import ru.hh.safebox.util.Util;
 import ru.hh.safebox.web.Response;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.*;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static java.nio.file.attribute.PosixFilePermission.GROUP_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.OTHERS_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_EXECUTE;
 
 public class Sandbox {
 
@@ -67,14 +76,28 @@ public class Sandbox {
     }
 
     private void copyScriptsToSharedDirectory() throws IOException {
-        Files.list(Paths.get("src/main/resources/scripts/"))
-                .forEach(f -> {
-                    try {
-                        Files.copy(f, runConfig.getSharedDir().resolve(f.getFileName()), StandardCopyOption.COPY_ATTRIBUTES);
-                    } catch (IOException e) {
-                        LOG.error("Can't copy file {} to dir {}", f, runConfig.getSharedDir(), e);
-                    }
-                });
+        copyScriptFromResources("java.sh", runConfig.getSharedDir(),
+                OWNER_EXECUTE, GROUP_EXECUTE, OTHERS_EXECUTE);
+        copyScriptFromResources("run.sh", runConfig.getSharedDir(),
+                OWNER_EXECUTE, GROUP_EXECUTE, OTHERS_EXECUTE);
+    }
+
+    private void copyScriptFromResources(String scriptName, Path sharedDir, PosixFilePermission... perms) throws IOException {
+        try (InputStream resourceStream = Sandbox.class
+                .getClassLoader()
+                .getResourceAsStream("scripts/" + scriptName);
+
+             FileOutputStream fos = new FileOutputStream(sharedDir.resolve(scriptName).toFile())) {
+
+            byte[] buf = new byte[2048];
+            int r;
+            while ((r = resourceStream.read(buf)) != -1) {
+                fos.write(buf, 0, r);
+
+            }
+
+            Files.setPosixFilePermissions(sharedDir.resolve(scriptName), new HashSet<>(Arrays.asList(perms)));
+        }
     }
 
 }
